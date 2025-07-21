94ff05e8f3143888f2ec1509d8566888dc2e0628
Christo K J <christokj.official@gmail.com>
2025-07-21 14:20:30 +0530

    Commands.ts file created

---
+++ ai-portfolio-client/src/utils/commands.ts
@@ -0,0 +1,6 @@
+help: "Available commands: about, projects, contact, sudo, clear",
+    about: "Hi, I'm Christo K.J., a full stack MERN developer...",
+        projects: "GitHub: https://github.com/christokj",
+            contact: "Email: your@email.com",
+                sudo: "🛑 You are not authorized! This incident will be reported.",
+};
