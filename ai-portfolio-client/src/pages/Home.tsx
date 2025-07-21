96904d842be450461487e31cb4b2aa89024f78db
Christo K J <christokj.official@gmail.com>
2025-07-21 14:37:46 +0530

    Home.tsx file created

---
+++ ai-portfolio-client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DefaultLayout from "../layouts/DefaultLayout";
+import { Terminal } from "../components/Terminal";
+
+const Home = () => {
+    return (
+        <DefaultLayout>
+            <Terminal />
+        </DefaultLayout>
+    );
+};
+
+export default Home;
